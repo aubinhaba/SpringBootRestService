4b4dd971e18339ded5a4edebb01bf259195de756
aubin <aubin.haba@gmail.com>
2022-03-10 22:03:53 +0100

    Linter added

--- src/main/java/com/bino/academy/service/LibraryService.java
+++ src/main/java/com/bino/academy/service/LibraryService.java
@@ -1,12 +1,11 @@
 package com.bino.academy.service;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.bino.academy.controller.Library;
 import com.bino.academy.repository.LibraryRepository;
+
 @Service
 public class LibraryService {
 	@Autowired
@@ -15,7 +14,7 @@ public class LibraryService {
 	{
 		if(isbn.startsWith("Z"))
 		{
-			return "old"+isbn+aisle;
+			return "old" + isbn + aisle;
 		}
 		return isbn+aisle;
 	}
