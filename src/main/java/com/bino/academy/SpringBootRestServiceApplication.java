59a037faa3c8599045bdc6baf8d1bc4c17cde568
aubin <aubin.haba@gmail.com>
2022-03-10 21:23:19 +0100

    remove unnecessary comments

--- src/main/java/com/bino/academy/SpringBootRestServiceApplication.java
+++ src/main/java/com/bino/academy/SpringBootRestServiceApplication.java
@@ -17,31 +17,4 @@ public class SpringBootRestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestServiceApplication.class, args);
 	}
-	
-	//@Override
-//	public void run(String[] args)
-//	{
-//		Library lib= repository.findById("fdsefr343").get();
-//		System.out.println(lib.getAuthor());
-//		Library en =new Library();
-//		en.setAisle(123);
-//		en.setAuthor("Rahul");
-//		en.setBook_name("Devops");
-//		en.setIsbn("lkhs");
-//		en.setId("lkhs123");
-//		//repository.save(en);
-//		List<Library> allrecords =repository.findAll();
-//		//
-//		for(Library item : allrecords)
-//		{
-//			System.out.println(item.getBook_name());
-//		}
-//		repository.delete(en);
-//		
-//		}
-	
-	
-	
-	   
-
 }
